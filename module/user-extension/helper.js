3b7cd4d33464215c7d490f334f13be11a66ecf8a
rakesh.k <rakesh.k@pacewisdom.com>
2020-12-02 15:53:12 +0530

    user profile api changes

--- module/user-extension/helper.js
+++ module/user-extension/helper.js
@@ -522,7 +522,7 @@ module.exports = class UserExtensionHelper {
                     delete requestedData.roles[pointerToRole]._id;
                 }
 
-                if (!userExtensionData[0]) {
+                if (userExtensionData) {
                     requestedData.userId = requestedData.updatedBy = userId;
 
                     requestedData.createdBy =
