339514b1ae91eb4a86c1c3b436f71329492eff1c
Akash Shah <aks301190@gmail.com>
2021-01-23 03:29:02 +0530

    iError fix.

--- module/entities/helper.js
+++ module/entities/helper.js
@@ -35,8 +35,7 @@ module.exports = class EntitiesHelper {
                 
                 if (findQuery != "all") {
                     queryObject = findQuery;
-                    if( queryObject._id && !gen.utils.isValidMongoId(queryObject._id.toString()) ) {
-                        
+                    if( queryObject._id && typeof queryObject._id != "object" && !gen.utils.isValidMongoId(queryObject._id.toString()) ) {
                         queryObject["registryDetails.locationId"] = queryObject._id;
                         delete queryObject._id
                     }
